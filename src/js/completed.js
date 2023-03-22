e81145192c4b2146146eae502b5f1147d1723f34
Emmanoel <emmanoelsveiga@gmail.com>
2023-03-21 21:31:27 -0300

    :sparkles: Alterações

--- src/js/completed.js
+++ src/js/completed.js
@@ -1,14 +1,7 @@
-const todoInput = document.querySelector("#add-tast-bar");
 const todoList = document.querySelector("#task-list");
-const deleteButtons = document.querySelectorAll("#delete-button");
-const checkButtons = document.querySelectorAll(
-  "#task-list button.check-button"
-);
 
 const history = localStorage.getItem("tasks");
 const filteredHistory = JSON.parse(history).filter((task) => task.done === 1);
-
-let oldInputValue;
 
 const removeFromLocalStorage = (taskField) => {
   let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
@@ -78,67 +71,6 @@ const deleteButtonListener = (button) => {
   }
 };
 
-const saveTodo = () => {
-  if (todoInput.value.trim() === "") {
-    return;
-  }
-
-  const taskObject = {
-    id: generateUUID(),
-    title: todoInput.value,
-    done: 0,
-  };
-
-  const todo = document.createElement("fieldset");
-  todo.classList.add("task-field");
-
-  const taskItem = document.createElement("div");
-  taskItem.classList.add("task-item");
-
-  const newCheckButton = createCheckButton();
-  taskItem.appendChild(newCheckButton);
-  addCheckButtonEventListener(newCheckButton);
-  const inputItem = createInputItem(taskObject.title);
-  inputItem.setAttribute("disabled", true);
-  taskItem.appendChild(inputItem);
-
-  const taskButtonDiv = document.createElement("div");
-  taskButtonDiv.classList.add("task-buttons");
-
-
-  const newDeleteButton = createDeleteButton();
-  taskButtonDiv.appendChild(newDeleteButton);
-  deleteButtonListener(newDeleteButton);
-
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.push(taskObject);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-
-  todo.appendChild(taskItem);
-  todo.appendChild(taskButtonDiv);
-
-  todo.setAttribute("data-task-id", taskObject.id); // Add this line
-
-  todoList.appendChild(todo);
-  todoInput.value = "";
-  feather.replace();
-};
-
-function changeTheme() {
-  const body = document.body;
-  const themeButton = document.querySelector("#sun");
-
-  if (body.classList.contains("dark")) {
-    body.classList.remove("dark");
-    themeButton.setAttribute("data-feather", "sun");
-  } else {
-    body.classList.add("dark");
-    themeButton.setAttribute("data-feather", "moon");
-  }
-
-  feather.replace();
-}
-
 if (history !== null && Object.values(history).length > 0) {
   const tasks = filteredHistory;
 
@@ -175,23 +107,4 @@ if (history !== null && Object.values(history).length > 0) {
     todoList.appendChild(todo);
     feather.replace();
   });
-}
-
-function generateUUID() {
-  let d = new Date().getTime();
-  if (
-    typeof performance !== "undefined" &&
-    typeof performance.now === "function"
-  ) {
-    d += performance.now(); // use high-precision timer if available
-  }
-  let uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-    /[xy]/g,
-    function (c) {
-      let r = (d + Math.random() * 16) % 16 | 0;
-      d = Math.floor(d / 16);
-      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
-    }
-  );
-  return uuid;
 }
--- src/js/deleted.js
+++ src/js/deleted.js
@@ -1,13 +1,6 @@
-const todoInput = document.querySelector("#add-tast-bar");
 const todoList = document.querySelector("#task-list");
-const deleteButtons = document.querySelectorAll("#delete-button");
-const checkButtons = document.querySelectorAll(
-  "#task-list button.check-button"
-);
 
 const history = localStorage.getItem("deletedTasks");
-
-let oldInputValue;
 
 const removeFromLocalStorage = (taskField) => {
   let tasks = JSON.parse(localStorage.getItem("deletedTasks")) || [];
@@ -115,51 +108,6 @@ const deleteButtonListener = (button) => {
   }
 };
 
-const saveTodo = () => {
-  if (todoInput.value.trim() === "") {
-    return;
-  }
-
-  const taskObject = {
-    id: generateUUID(),
-    title: todoInput.value,
-    done: 0,
-  };
-
-  const todo = document.createElement("fieldset");
-  todo.classList.add("task-field");
-
-  const taskItem = document.createElement("div");
-  taskItem.classList.add("task-item");
-
-  const newCheckButton = createCheckButton();
-  taskItem.appendChild(newCheckButton);
-  addCheckButtonEventListener(newCheckButton);
-  const inputItem = createInputItem(taskObject.title);
-  inputItem.setAttribute("disabled", true);
-  taskItem.appendChild(inputItem);
-
-  const taskButtonDiv = document.createElement("div");
-  taskButtonDiv.classList.add("task-buttons");
-
-  const newDeleteButton = createDeleteButton();
-  taskButtonDiv.appendChild(newDeleteButton);
-  deleteButtonListener(newDeleteButton);
-
-  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-  tasks.push(taskObject);
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-
-  todo.appendChild(taskItem);
-  todo.appendChild(taskButtonDiv);
-
-  todo.setAttribute("data-task-id", taskObject.id); // Add this line
-
-  todoList.appendChild(todo);
-  todoInput.value = "";
-  feather.replace();
-};
-
 function changeTheme() {
   const body = document.body;
   const themeButton = document.querySelector("#sun");
@@ -215,23 +163,4 @@ if (history !== null && Object.values(history).length > 0) {
     todoList.appendChild(todo);
     feather.replace();
   });
-}
-
-function generateUUID() {
-  let d = new Date().getTime();
-  if (
-    typeof performance !== "undefined" &&
-    typeof performance.now === "function"
-  ) {
-    d += performance.now(); // use high-precision timer if available
-  }
-  let uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-    /[xy]/g,
-    function (c) {
-      let r = (d + Math.random() * 16) % 16 | 0;
-      d = Math.floor(d / 16);
-      return (c == "x" ? r : (r & 0x3) | 0x8).toString(16);
-    }
-  );
-  return uuid;
 }
